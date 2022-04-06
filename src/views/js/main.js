70af9e2173aae42e4b84f4f77b6af7a8904857da
paulpetelski <petelskipaul@gmail.com>
2022-04-06 13:48:01 -0700

    added api call

--- src/views/js/main.js
+++ src/views/js/main.js
@@ -1,1 +1,16 @@
-// Temporary content
+function getWeather(zipCode){
+    let apiKey = 'd5064516cc8bdc14fcbe9857888299c4';
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${zipCode}&appid=${apiKey}&units=imperial`;
+
+    fetch(url)
+    .then((resp) => {return resp.json()})      // get json data
+    .then((data) => displayWeather(data));      // send data to method for display
+}
+
+function displayWeather(weather){
+    let city = weather.name;
+    let temp = weather.main.temp + "°F";
+    console.log("City: " + city + "\n" + "Temp: " + temp);
+}
+
+getWeather(60622);
